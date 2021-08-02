7640364a2ee58ba95ab1801a10d2f0bf1859e20f
Rahul Nagubadi <rahul.nagubandi@multiscale.io>
2021-08-02 11:16:06 +0530

    sample commi

--- model.py
+++ model.py
@@ -19,3 +19,4 @@ y_pred = classifier.predict(X_test)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 pickle.dump(classifier, open('model.pkl','wb'))
 pickle.dump(sc, open('scaler.pkl', 'wb'))
+
